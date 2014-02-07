6f35aaa6c7e8ee3271e67a4b8b7b5300d2d1bbab
Jedford Seculles <jfjade18@yahoo.com>
2014-02-08 01:51:44 +0800

    change to v0.0.3

--- lib/basecamp_ninja/version.rb
+++ lib/basecamp_ninja/version.rb
@@ -2,8 +2,8 @@ module BasecampNinja
   class Version
     MAJOR = 0 unless defined? BasecampNinja::MAJOR
     MINOR = 0 unless defined? BasecampNinja::MINOR
-    PATCH = 2 unless defined? BasecampNinja::PATCH
-    PRE   = 1 unless defined? BasecampNinja::PRE
+    PATCH = 3 unless defined? BasecampNinja::PATCH
+    PRE   = 0 unless defined? BasecampNinja::PRE
 
     class << self
 
